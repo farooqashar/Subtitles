b273f8ccb9a45670aa91c46e04a802cabde5d708
Ashar Farooq <afarooq@mit.edu>
2023-06-26 10:34:28 +0200

    adding first set of files raw

---
+++ testvid.py
@@ -0,0 +1,63 @@
+# from moviepy.video.tools.subtitles import SubtitlesClip
+# from moviepy.editor import *
+
+# video = VideoFileClip("bruh.mp4")
+# subtitles = SubtitlesClip("subtitles.srt")
+
+# subtitles = subtitles.set_duration(video.duration)
+# subtitles = subtitles.set_position(("center", "bottom"))
+# subtitles.on_color(size=(100,100))
+
+
+# final_video = CompositeVideoClip([video, subtitles])
+# final_video.write_videofile("output.mp4")
+
+import pysrt
+from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
+
+# Convert a time object from the SRT file(i.e. 00:00:40,822) into total seconds
+def time_to_seconds(time):
+
+    hours, mins, seconds, milliseconds =  time
+    # Convert each metric to seconds 
+    return (hours * 3600) + (mins * 60) + seconds + (milliseconds / 1000)
+
+# Creating TextClip objects for each subtitle in the SRT file with some styling
+def create_subtitle_text_clips(subtitles, video_size):
+    subtitle_clips = []
+
+    for subtitle in subtitles:
+        # Getting time information
+        start_time = time_to_seconds(subtitle.start)
+        end_time = time_to_seconds(subtitle.end)
+        subtitle_duration = end_time - start_time
+
+        video_width, video_height = video_size
+        size=(video_width * 3/4, None)
+
+        # Creating subtitle TextClip with some attributes(duration, start time, styling)
+        other_language, english = subtitle.text.split("\n")
+
+        other_language_text_clip = TextClip(other_language, fontsize=24, font="Arial", color="red", bg_color = 'black',size=size, method='caption').set_start(start_time).set_duration(subtitle_duration)
+        english_clip = TextClip(english, fontsize=18, font="STIXGeneral-Italic", color="white", bg_color = 'black',size=size, method='caption').set_start(start_time).set_duration(subtitle_duration)
+
+        subtitle_x_position = 'center'
+        # Positioning and handling the subtitle for the non-English language
+        subtitle_y_position_non_eng = video_height * 8/10
+        text_position_non_eng = (subtitle_x_position, subtitle_y_position_non_eng)   
+        subtitle_clips.append(other_language_text_clip.set_position(text_position_non_eng))
+
+        # Positioning and handling the subtitle for the English language
+        subtitle_y_position_eng = (video_height * 8/10) + other_language_text_clip.size[1]
+        text_position_eng = (subtitle_x_position, subtitle_y_position_eng)   
+        subtitle_clips.append(english_clip.set_position(text_position_eng))
+
+    return subtitle_clips
+
+video = VideoFileClip("bruh.mp4")
+subtitles = pysrt.open("subtitles.srt")
+
+subtitle_clips = create_subtitle_text_clips(subtitles,video.size)
+
+final_video = CompositeVideoClip([video] + subtitle_clips)
+final_video.write_videofile("output.mp4")
